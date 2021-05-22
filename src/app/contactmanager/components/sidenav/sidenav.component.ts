e6306f9dccecf00119c93f1df73043dcacbc4daa
unknown <mrgruby@gmail.com>
2021-05-23 00:06:44 +0200

    Finished module 5

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,15 +1,21 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+  const SMALL_WIDTH_BREAKPOINT = 720;
+  @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private breakPointObserver: BreakpointObserver) { }
+  public isScreenSmall: boolean | undefined;
 
   ngOnInit(): void {
+    this.breakPointObserver
+    .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+    .subscribe((state: BreakpointState) => this.isScreenSmall = state.matches)
   }
-
 }
--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ToolbarComponent implements OnInit {
 
   constructor() { }
+
+  @Output() toggleSideNav = new EventEmitter<void>();
 
   ngOnInit(): void {
   }

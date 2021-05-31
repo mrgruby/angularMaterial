9846f0561e93ad31b44b392d4fd1533bce2548bd
unknown <mrgruby@gmail.com>
2021-05-31 21:38:31 +0200

    Begin module 8

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
@@ -13,9 +14,11 @@ export class NotesComponent implements OnInit, AfterViewInit {
   constructor() { }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   @Input() notes: Note[] = [];
@@ -27,6 +30,7 @@ export class NotesComponent implements OnInit, AfterViewInit {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
+  //Called by each key-up inside the filter textbox. The event contains the textbox, and its value is used to filter the datasource for the table.
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
